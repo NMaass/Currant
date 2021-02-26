e89d914d4e30a6236ab6a072a11fd9a35b61e0fa
nmaass <nicholasfmaassen@gmail.com>
2021-02-26 11:50:17 -0500

    Added basic aws config and npm config fixes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
 import App from './App';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
